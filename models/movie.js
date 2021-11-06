104ca547a4015a5460c4664b57f084e2e499077e
smj2021 <mjschwartz515@gmail.com>
2021-11-05 20:47:30 -0400

    add id and url props to movieSchema

--- models/movie.js
+++ models/movie.js
@@ -2,3 +2,14 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema;
 
+const movieSchema = new Schema({
+    mal_id: {
+        type: Number,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    
+})
