3b5b1c3c6619f96075fb429d82f6f1f9df2a7bb4
smj2021 <mjschwartz515@gmail.com>
2021-11-06 17:16:04 -0400

    mutiple search results rendering

--- src/pages/AnimeInfo/AnimeInfo.js
+++ src/pages/AnimeInfo/AnimeInfo.js
@@ -5,20 +5,9 @@ class AnimeInfo extends Component {
         console.log(this.props.animes)
         return (
             <div>
-                <h1>{this.props.animes.results[0].title}</h1>
-                <img src={this.props.animes.results[0].image_url} alt="" />
-                <p><b>Type: </b>{this.props.animes.results[0].type}</p>
-                {this.props.animes.results[0].type === 'TV'
-                    ? <p><b>Episodes: </b>{this.props.animes.results[0].episodes}</p>
-                    : ''
-                }
-                {this.props.animes.results[0].airing
-                    ? <p><b>Airing: </b>{this.props.animes.results[0].start_date} - present</p>
-                    : <p><b>Aired: </b>{this.props.animes.results[0].start_date} to {this.props.animes.results[0].end_date}</p>
-                }
-                <p><b>Rated: </b>{this.props.animes.results[0].rated}</p>
-                <p><b>Synopsis: </b>{this.props.animes.results[0].synopsis}</p>
-                <a href={this.props.animes.results[0].url}>More info</a>
+                {this.props.animes.map(anime => (
+                    <img src={anime.image_url} alt="" />
+                ))}
             </div>
         )
     }
