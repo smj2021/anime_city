210e12d991aaacb423fdc1865581c99528288a85
smj2021 <mjschwartz515@gmail.com>
2021-11-06 16:59:00 -0400

    refactor AnimeInfo.js for multiple results

--- src/pages/AnimeInfo/AnimeInfo.js
+++ src/pages/AnimeInfo/AnimeInfo.js
@@ -4,20 +4,20 @@ class AnimeInfo extends Component {
     render() {
         return (
             <div>
-                <h1>{this.props.anime.results[0].title}</h1>
-                <img src={this.props.anime.results[0].image_url} alt="" />
-                <p><b>Type: </b>{this.props.anime.results[0].type}</p>
-                {this.props.anime.results[0].type === 'TV'
-                    ? <p><b>Episodes: </b>{this.props.anime.results[0].episodes}</p>
+                <h1>{this.props.animes.results[0].title}</h1>
+                <img src={this.props.animes.results[0].image_url} alt="" />
+                <p><b>Type: </b>{this.props.animes.results[0].type}</p>
+                {this.props.animes.results[0].type === 'TV'
+                    ? <p><b>Episodes: </b>{this.props.animes.results[0].episodes}</p>
                     : ''
                 }
-                {this.props.anime.results[0].airing
-                    ? <p><b>Airing: </b>{this.props.anime.results[0].start_date} - present</p>
-                    : <p><b>Aired: </b>{this.props.anime.results[0].start_date} to {this.props.anime.results[0].end_date}</p>
+                {this.props.animes.results[0].airing
+                    ? <p><b>Airing: </b>{this.props.animes.results[0].start_date} - present</p>
+                    : <p><b>Aired: </b>{this.props.animes.results[0].start_date} to {this.props.animes.results[0].end_date}</p>
                 }
-                <p><b>Rated: </b>{this.props.anime.results[0].rated}</p>
-                <p><b>Synopsis: </b>{this.props.anime.results[0].synopsis}</p>
-                <a href={this.props.anime.results[0].url}>More info</a>
+                <p><b>Rated: </b>{this.props.animes.results[0].rated}</p>
+                <p><b>Synopsis: </b>{this.props.animes.results[0].synopsis}</p>
+                <a href={this.props.animes.results[0].url}>More info</a>
             </div>
         )
     }
