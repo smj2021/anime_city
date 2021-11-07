1407bdb8b00b8a323d4f4540d5e6d6dc34b60d4d
smj2021 <mjschwartz515@gmail.com>
2021-11-06 22:10:49 -0400

    add details view img

--- src/pages/AnimeDetails/AnimeDetails.js
+++ src/pages/AnimeDetails/AnimeDetails.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react'
+
 class AnimeDetails extends Component {
-render(){
-    return(
-        <div>
-            <h1>{this.props.results.title}</h1>
-        </div>
-    )
-}
+    render() {
+        return (
+            <div>
+                <h1>{this.props.animes.results.title}</h1>
+                <img src={this.props.animes.results.image_url} alt="" />
+            </div>
+        )
+    }
 }
 
 export default AnimeDetails;
