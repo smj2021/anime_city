782e701ffb8654f6314ae9fac4415e8e5f4640e5
smj2021 <mjschwartz515@gmail.com>
2021-11-06 22:08:51 -0400

    add details view title

--- src/pages/AnimeDetails/AnimeDetails.js
+++ src/pages/AnimeDetails/AnimeDetails.js
@@ -1,5 +1,11 @@
 class AnimeDetails extends Component {
-
+render(){
+    return(
+        <div>
+            <h1>{this.props.results.title}</h1>
+        </div>
+    )
+}
 }
 
 export default AnimeDetails;
