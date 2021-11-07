e453da58b4dcd1c78ed41669088d42d326e15e60
smj2021 <mjschwartz515@gmail.com>
2021-11-06 20:54:57 -0400

    move img render inside div

--- src/pages/AnimeInfo/AnimeInfo.js
+++ src/pages/AnimeInfo/AnimeInfo.js
@@ -4,11 +4,13 @@ class AnimeInfo extends Component {
     render() {
         console.log(this.props.animes)
         return (
-            <div>
+            <>
                 {this.props.animes.map(anime => (
-                    <img src={anime.image_url} alt="" />
+                    <div>
+                        <img src={anime.image_url} alt="" />
+                    </div>
                 ))}
-            </div>
+            </>
         )
     }
 }
