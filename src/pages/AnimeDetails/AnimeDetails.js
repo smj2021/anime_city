6ff35d769b1ddc8800980654fad72f76845dbc75
smj2021 <mjschwartz515@gmail.com>
2021-11-06 22:20:48 -0400

    add type and air date to details view

--- src/pages/AnimeDetails/AnimeDetails.js
+++ src/pages/AnimeDetails/AnimeDetails.js
@@ -6,6 +6,14 @@ class AnimeDetails extends Component {
             <div>
                 <h1>{this.props.animes.results.title}</h1>
                 <img src={this.props.animes.results.image_url} alt="" />
+                <p><b>Type:</b>{this.props.animes.results.type}</p>
+                {this.props.animes.results.type === "TV"
+                    ? <p><b>Episodes: </b>{this.props.animes.results.episodes}</p>
+                    : ''}
+                {this.props.animes.results.airing
+                    ? <p><b>Airing: </b>{this.props.animes.results.start_date} - present</p>
+                    : <p><b>Aired: </b>{this.props.animes.results.start_date} to {this.props.animes.results.end_date}</p>
+                }
             </div>
         )
     }
