58c984f11832ddf1b12359b58443fad15d3013b8
smj2021 <mjschwartz515@gmail.com>
2021-11-08 22:38:07 -0500

    add return to AnimeLists

--- src/pages/AnimeLists/AnimeLists.js
+++ src/pages/AnimeLists/AnimeLists.js
@@ -6,6 +6,13 @@ function AnimeLists(props) {
     for (let i = 0; i < props.lists; i++) {
         lists.push(<List key={i} />)
     }
+
+    return (
+        <div>
+            {lists}
+        </div>
+    )
 }
+
 
 export default AnimeLists;
