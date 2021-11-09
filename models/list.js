5adfac8541f916ae340740adced99b4fa243652f
smj2021 <mjschwartz515@gmail.com>
2021-11-09 13:27:15 -0500

    remove lists link from navbar

--- models/list.js
+++ models/list.js
@@ -4,8 +4,14 @@ const Schema = mongoose.Schema
 
 const listSchema = new Schema ({
     name: String,
-    profile: ref - Profile,
-    shows: [ref - show]
+    profile:{
+        ref: 'Profile'
+    },
+    shows: [{
+        ref: 'Show'
+    }]
+},{
+    timestamps: true,
 })
 
 const List = mongoose.model('List', listSchema)
