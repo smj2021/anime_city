6c6df3c7a936b3bf2e63b07384b17df14730ede3
giorgiovh <jorgevonhoroch@gmail.com>
2021-11-10 11:37:24 -0500

    deletes old code

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -22,15 +22,6 @@ function create(req, res) {
         })
 }
 
-// function create(req, res) {
-//     console.log('id', req.body.id);
-//     Favorite.create({id:req.body.id}, function(err, fav) {
-//         if(err) return res.json({ok: false});
-//         console.log(fav);
-//         res.json({ok: true, obj: fav})
-//     })
-// }
-
 export {
     create
 }
