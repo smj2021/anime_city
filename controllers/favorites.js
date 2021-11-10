dd8241673a8a1e96a650eb16e358b9e54d197c34
giorgiovh <jorgevonhoroch@gmail.com>
2021-11-09 21:26:04 -0500

    adds addedBy property to favoriteSchema

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,10 +1,19 @@
 import { Favorite } from '../models/favorite.js'
+import { Profile } from '../models/profile.js'
 
 function create(req, res) {
     console.log('req.user is: ', req.user);
     console.log('req.user.profile is: ', req.user.profile);
-    console.log('req.body.id is: ', req.body.id);
+    // console.log('req.body.id is: ', req.body.id);
+    req.body.addedBy = req.user.profile
+    console.log('req.body is: ', req.body);
     Favorite.create((req.body))
+        .then(favorite => {
+            // console.log('req.user.profile is: ', req.user.profile);
+            Profile.findById(req.user.profile)
+                .populate('favorites')
+                .then(profile => console.log('profile is: ', profile))
+        })
 }
 
 // function create(req, res) {
