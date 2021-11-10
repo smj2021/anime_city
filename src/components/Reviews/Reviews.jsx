65d75d7b5594f348ba0f621e40ea84a7e33668f2
smj2021 <mjschwartz515@gmail.com>
2021-11-09 21:18:53 -0500

    add ratingValue based on index value

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -4,8 +4,9 @@ import { FaStar } from 'react-icons/fa'
 const StarRating = () => {
     return <div>
         {[...Array(5)].map((star, idx) => {
+            const ratingValue = idx + 1
             return <label>
-                <input hidden type="radio" name="rating" />
+                <input hidden type="radio" name="rating" value={ratingValue} />
                 <FaStar className="star" size={50} />
             </label>
         })}
