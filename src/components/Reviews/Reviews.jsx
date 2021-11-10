ac59125a8878998a514f5726713e460054261050
smj2021 <mjschwartz515@gmail.com>
2021-11-09 21:34:45 -0500

    add star color switch ternary

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -8,8 +8,14 @@ const StarRating = () => {
         {[...Array(5)].map((star, idx) => {
             const ratingValue = idx + 1
             return <label>
-                <input hidden type="radio" name="rating" value={ratingValue} onClick={() => setRating(ratingValue)} />
-                <FaStar className="star" size={50} />
+                <input
+                    hidden
+                    type="radio"
+                    name="rating"
+                    value={ratingValue}
+                    onClick={() => setRating(ratingValue)}
+                />
+                <FaStar className="star" color={ratingValue <= rating ? '#ffc107' : '#e4e5e9'} size={50} />
             </label>
         })}
     </div>
