112e92625cbfcbbdffd5af556dc44f1853036547
smj2021 <mjschwartz515@gmail.com>
2021-11-09 21:43:41 -0500

    add onMouseEnter setHove on stars

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,6 +3,8 @@ import { FaStar } from 'react-icons/fa'
 
 const StarRating = () => {
     const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+
 
     return <div>
         {[...Array(5)].map((star, idx) => {
@@ -14,6 +16,7 @@ const StarRating = () => {
                     name="rating"
                     value={ratingValue}
                     onClick={() => setRating(ratingValue)}
+                    onMouseEnter={() => setHover(ratingValue)}
                 />
                 <FaStar className="star" color={ratingValue <= rating ? '#ffc107' : '#e4e5e9'} size={50} />
             </label>
