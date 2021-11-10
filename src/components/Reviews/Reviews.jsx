bdab0b485508e3f80064630a34a22051e32f0ec1
smj2021 <mjschwartz515@gmail.com>
2021-11-09 21:16:49 -0500

    add index iterator to map call

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,11 +3,11 @@ import { FaStar } from 'react-icons/fa'
 
 const StarRating = () => {
     return <div>
-        {[...Array(5)].map((star) => {
+        {[...Array(5)].map((star, idx) => {
             return <label>
-                <input type="radio" name="rating" />
-                <FaStar size={50} />
-                </label>
+                <input hidden type="radio" name="rating" />
+                <FaStar className="star" size={50} />
+            </label>
         })}
     </div>
 }
