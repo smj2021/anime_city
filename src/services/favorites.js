64c5f6930b5fa9d8129a758909d64d3775c8d8b2
giorgiovh <jorgevonhoroch@gmail.com>
2021-11-11 17:17:18 -0500

    adds headers to deleteOne func

--- src/services/favorites.js
+++ src/services/favorites.js
@@ -1,7 +1,13 @@
 const BASE_URL = 'http://localhost:3001/api/favorites'
 
 function deleteOne(id) {
-    return fetch(`${BASE_URL}/${id}`, {method: 'DELETE'})
+    return fetch(`${BASE_URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token')
+        }
+    })
     .then(res => res.json())
 }
 
