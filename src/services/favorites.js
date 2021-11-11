c4d6249b18d098c11a1926a97ab4e8ce838d008d
giorgiovh <jorgevonhoroch@gmail.com>
2021-11-11 15:34:29 -0500

    adds logic for deleteOne function

--- src/services/favorites.js
+++ src/services/favorites.js
@@ -1,7 +1,8 @@
 const BASE_URL = 'http://localhost:3001/api/favorites'
 
 function deleteOne(id) {
-    
+    return fetch(`${BASE_URL}/${id}`, {method: 'DELETE'})
+    .then(res => res.json())
 }
 
 export {
