4ed3005e7a52caeaf590c1c249ff99d0dafe0c20
giorgiovh <jorgevonhoroch@gmail.com>
2021-11-10 21:41:32 -0500

    gets images to render in favorites page

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -27,7 +27,12 @@ class Favorites extends Component {
                 <h1>Favorites</h1>
                 {/* render gets called before we fetch so we first need to check wen have the data before we map */}
                 {this.state.profile.favorites && this.state.profile.favorites.map((favorite, idx) => {
-                    return <p key={idx}>{favorite.title}</p>
+                    return (
+                        <div>
+                            <h2 key={idx}>{favorite.title}</h2>
+                            <img src={favorite.image} alt="" />
+                        </div>
+                    )
                 })}
             </div>
         );
