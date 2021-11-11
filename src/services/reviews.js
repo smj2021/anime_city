8c9bb45c7153a471e74a1c17d7a2fde7e0ffd274
smj2021 <mjschwartz515@gmail.com>
2021-11-11 15:49:57 -0500

    add create review service

---
+++ src/services/reviews.js
@@ -0,0 +1,10 @@
+const BASE_URL = '/api/reviews';
+
+const create = async newReviewData => {
+    return fetch(BASE_URL, {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newReviewData)
+    }).then(res => res.json());
+}
+
