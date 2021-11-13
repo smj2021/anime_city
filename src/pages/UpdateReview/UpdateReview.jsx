96814fd5ad1605868cddc41466f7b6ef8d611fad
smj2021 <mjschwartz515@gmail.com>
2021-11-13 14:41:27 -0500

    export updatereview

--- src/pages/UpdateReview/UpdateReview.jsx
+++ src/pages/UpdateReview/UpdateReview.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class UpdateReview extends Component{
+class UpdateReview extends Component {
     state = {
         invalidForm: false,
         formData: this.props.location.state.review
     };
 
-    
+    formRef = React.createRef();
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.handleUpdateReview(this.state.formData);
+    };
+
+    handleChange = (e) => {
+        const formData = { ...this.state.formData, [e.target.content]: e.target.value }
+    }
 }
+
+export default UpdateReview;
