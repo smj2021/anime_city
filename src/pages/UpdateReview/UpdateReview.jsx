b6406efdfa93a33488d166eaff492124036218f2
smj2021 <mjschwartz515@gmail.com>
2021-11-13 14:31:59 -0500

    add updatereview state

--- src/pages/UpdateReview/UpdateReview.jsx
+++ src/pages/UpdateReview/UpdateReview.jsx
@@ -1,3 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+class UpdateReview extends Component{
+    state = {
+        invalidForm: false,
+        formData: this.props.location.state.review
+    };
+
+    
+}
